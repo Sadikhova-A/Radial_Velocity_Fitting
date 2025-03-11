b7d4b0a52853be52c61dd529a4e9182bc606c2ca
Sadikhova-A <aylinsadixova@gmail.com>
2025-03-11 11:43:26 +0000

    Update ONE_data_gen.py
    Added noise to RV data, somehow forgot to do that earlier

--- ONE_data_gen.py
+++ ONE_data_gen.py
@@ -2,15 +2,20 @@ import numpy as np
 import pandas as pd
 
 # Parameters
+N  50
 P1 = 25
 K1 = np.random.normal(9, 4)
 phi1 = np.random.uniform(0, 2*np.pi)
 
 # Generating time values
-t = np.sort(np.random.uniform(0, 100, 50))
+t = np.sort(np.random.uniform(0, 100, N))
 
 # Model for ONE planet
-vel = K1 * np.sin(2 * np.pi * t / P1 + phi1)
+vr = K1 * np.sin(2 * np.pi * t / P1 + phi1)
+
+# Adding Noise
+noise = 2.2
+vel =  vr + np.random.normal(0, noise, N)
 
 # Errors
 errvel = np.random.normal(2, 0.5, size=len(t))
